d5c6d804926093b14a6b2f0364acb6c7aa61a04f
Bibliofile <bibliofilegit@gmail.com>
2017-01-06 15:22:46 -0700

    Trigger messages

---
+++ dev/messages/trigger/index.js
@@ -0,0 +1,82 @@
+const ui = require('app/ui');
+
+const storage = require('app/libraries/storage');
+const hook = require('app/libraries/hook');
+const helpers = require('app/messages/helpers');
+const settings = require('app/settings');
+
+const STORAGE_ID = 'triggerArr';
+
+var tab = ui.addTab('Trigger', 'messages');
+tab.innerHTML = INCLUDE_FILE('/dev/messages/trigger/tab.html');
+
+module.exports = {
+    tab,
+    save,
+    addMessage,
+};
+
+var triggerMessages = storage.getObject(STORAGE_ID, []);
+triggerMessages.forEach(addMessage);
+
+
+function addMessage(msg = {}) {
+    ui.buildContentFromTemplate('#tTemplate', '#tMsgs', [
+        {selector: 'option', remove: ['selected'], multiple: true},
+        {selector: '.m', text: msg.message || ''},
+        {selector: '.t', value: msg.trigger || ''},
+        {selector: '[data-target="joins_low"]', value: msg.joins_low || 0},
+        {selector: '[data-target="joins_high"]', value: msg.joins_high || 9999},
+        {selector: `[data-target="group"] [value="${msg.group || 'All'}"]`, selected: 'selected'},
+        {selector: `[data-target="not_group"] [value="${msg.not_group || 'Nobody'}"]`, selected: 'selected'}
+    ]);
+}
+
+
+function save() {
+    triggerMessages = [];
+    Array.from(tab.querySelectorAll('.third-box')).forEach(container => {
+        if (!container.querySelector('.m').value || !container.querySelector('.t').value) {
+            return;
+        }
+
+        triggerMessages.push({
+            message: container.querySelector('.m').value,
+            trigger: container.querySelector('.t').value,
+            joins_low: +container.querySelector('[data-target="joins_low"]').value,
+            joins_high: +container.querySelector('[data-target="joins_high"]').value,
+            group: container.querySelector('[data-target="group"]').value,
+            not_group: container.querySelector('[data-target="not_group"]').value,
+        });
+    });
+
+    storage.set(STORAGE_ID, triggerMessages);
+}
+
+function triggerMatch(trigger, message) {
+    if (settings.regexTriggers) {
+        try {
+            return new RegExp(trigger, 'i').test(message);
+        } catch (e) {
+            ui.notify(`Skipping trigger '${trigger}' as the RegEx is invaild.`);
+            return false;
+        }
+    }
+    return new RegExp(
+            trigger
+                .replace(/([.+?^=!:${}()|\[\]\/\\])/g, "\\$1")
+                .replace(/\*/g, ".*"),
+            'i'
+        ).test(message);
+}
+
+// Watch for triggers
+hook.on('world.message', function checkTriggers(name, message) {
+    var totalAllowed = settings.maxResponses;
+    triggerMessages.forEach(msg => {
+        if (totalAllowed && helpers.checkJoinsAndGroup(msg, name) && triggerMatch(msg.trigger, message)) {
+            helpers.buildAndSendMessage(msg.message, name);
+            totalAllowed--;
+        }
+    });
+});
