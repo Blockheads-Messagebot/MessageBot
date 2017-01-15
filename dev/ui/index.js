42e5ef9ca791e4ad0e66ab608d8501a3b9e8d94f
Bibliofile <bibliofilegit@gmail.com>
2017-01-14 21:53:14 -0700

    Correctly require the bot's console

--- dev/ui/index.js
+++ dev/ui/index.js
@@ -9,7 +9,7 @@ Object.assign(
 );
 
 // Functions which are no longer contained in this module, but are retained for now for backward compatability.
-const write = require('console/exports').write;
+const write = require('../console/exports').write;
 module.exports.addMessageToConsole = function(msg, name = '', nameClass = '') {
     console.warn('ui.addMessageToConsole has been depricated. Use ex.console.write instead.');
     write(msg, name, nameClass);
